343fcf852dd7041a2e8b940ab28f9a4d90829087
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-05-29 16:15:19 +0700

    Initial Commit

---
+++ Cara/CaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaraApp.swift
+//  Cara
+//
+//  Created by Muhammad Akbar Reishandy on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
