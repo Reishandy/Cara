fdbbc298a6359980ebb71317114683a445ed2bac
suyeonlee-hub <syjaydenlee@gmail.com>
2026-06-03 12:28:30 +0800

    Update xcode.yml to remove push event
    Remove push trigger for main branch in workflow

--- Cara/Views/Components/VitalPillView.swift
+++ Cara/Views/Components/VitalPillView.swift
@@ -8,45 +8,37 @@
 import SwiftUI
 
 struct VitalPillView: View {
-	let title: String
 	let unit: String
 	let systemIcon: String
 	
 	// FIXME: Dynamic type using generic T?
 	@Binding var value: String
 	
-	// FIXME: Change color
 	var body: some View {
-		HStack(spacing: 10) {
-			ZStack {
-				Image(systemName: systemIcon)
-				
-				Circle()
-					.frame(width: 40)
-					.foregroundStyle(.black.opacity(0.5))
-			}
-			
-			VStack(alignment: .leading, spacing: 4) {
-				Text(title)
-					.bold()
-				
-				HStack(alignment: .center, spacing: 4) {
-					TextField(". . .", text: $value)
-						.frame(width: 20)
-						.padding(.leading, 4)
-						.padding(.horizontal, 6)
-						.padding(.vertical, 2)
-						.overlay(
-							RoundedRectangle(cornerRadius: 10)
-								.stroke(.black.opacity(0.5), lineWidth: 1)
-						)
-					
-					Text(unit)
-						.font(.callout)
-						.foregroundStyle(.black.opacity(0.5))
-				}
-			}
-		}
+        VStack(alignment: .center, spacing: 10) {
+            Image(systemName: systemIcon)
+                .font(.title2)
+                .foregroundStyle(.appPrimary)
+            
+            TextField("- -", text: $value)
+                .font(.subheadline)
+                .bold()
+                .foregroundStyle(.appSecondary)
+                .multilineTextAlignment(.center)
+                .keyboardType(.decimalPad)
+            
+            Text(unit)
+                .font(.caption)
+                .foregroundStyle(.appPrimary)
+            
+        }
+        .frame(maxWidth: .infinity)
+        .padding(12)
+        .background(Color.appThird.opacity(0.2))
+        .cornerRadius(12)
+        
+        
+
 		.frame(maxWidth: .infinity)
 	}
 }
@@ -55,7 +47,7 @@ struct VitalPillView: View {
 	// FIXME: Updatable preview
 	
 	VitalPillView(
-		title: "Temperature", unit: "celcius", systemIcon: "thermometer.variable", value: .constant("")
+		unit: "celcius", systemIcon: "thermometer.variable", value: .constant("")
 	)
 	.padding()
 }
