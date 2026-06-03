80b3b212a0f8a2e7cd8149eb9d6c029ad23529db
suyeonlee-hub <syjaydenlee@gmail.com>
2026-06-03 16:28:49 +0800

    Update xcode.yml to remove push event (#9)
    Remove push trigger for main branch in workflow
    
    Co-authored-by: Muhammad Akbar Reishandy <akbar@reishandy.id>

--- Cara/Views/Screens/RoutineDetailView.swift
+++ Cara/Views/Screens/RoutineDetailView.swift
@@ -16,6 +16,11 @@ struct RoutineDetailView: View {
     @Bindable var routine: Routine
     @State private var currentElement: RoutineDetailElement = .task
     
+    @State private var vitalFilledDate: Date? = nil
+    @State private var bloodPressure: String = ""
+    @State private var heartRate: String = ""
+    @State private var temperature: String = ""
+    @State private var oxygenLevel: String = ""
     
     @State private var noteText: String = ""
     @State private var lastEditedDate: Date? = nil
@@ -39,93 +44,113 @@ struct RoutineDetailView: View {
             
             switch currentElement {
             case .task:
-                // FIXME: Vitals color matching
-                VStack(spacing: 24) {
-                    HStack {
-                        Text("Check Vitals")
-                            .font(.title)
-                            .bold()
-                        
-                        Image(systemName: "questionmark.circle")
-                        
-                        Spacer()
-                    }
                     
                     VStack(spacing: 24) {
                         HStack {
-                            VitalPillView(
-                                title: "Blood Pressure",
-                                unit: " / 80 mmhg",
-                                systemIcon: "thermometer.variable",
-                                value: .constant("") // FIXME: Change this
-                            )
+                            Text("Check Vitals")
+                                .font(.title)
+                                .bold()
                             
-                            VitalPillView(
-                                title: "Blood Pressure",
-                                unit: " / 80 mmhg",
-                                systemIcon: "thermometer.variable",
-                                value: .constant("") // FIXME: Change this
-                            )
+                            Image(systemName: "questionmark.circle")
+                            
+                            Spacer()
                         }
                         
-                        HStack {
-                            VitalPillView(
-                                title: "Blood Pressure",
-                                unit: " / 80 mmhg",
-                                systemIcon: "thermometer.variable",
-                                value: .constant("") // FIXME: Change this
-                            )
+                        VStack {
+                            HStack(spacing: 8) {
+                                    VitalPillView(
+                                        unit: "mm HG",
+                                        systemIcon: "blood.pressure.cuff",
+                                        value: $bloodPressure
+                                        
+                                    ).onChange(of: bloodPressure) {
+                                        vitalFilledDate = Date()
+                                    }
+                                    
+                                    VitalPillView(
+                                        unit: "bpm",
+                                        systemIcon: "waveform.path.ecg",
+                                        value: $heartRate
+                                    ).onChange(of: heartRate) {
+                                        vitalFilledDate = Date()
+                                    }
+                                    
+                                    VitalPillView(
+                                        unit: "℃",
+                                        systemIcon: "thermometer.variable",
+                                        value: $temperature
+                                    ).onChange(of: temperature) {
+                                        vitalFilledDate = Date()
+                                    }
+                               
+                                    VitalPillView(
+                                        
+                                        unit: " %",
+                                        systemIcon: "lungs",
+                                        value: $oxygenLevel
+                                    )
+                                    .onChange(of: oxygenLevel) {
+                                        vitalFilledDate = Date()
+                                        }
+                                    }
                             
-                            VitalPillView(
-                                title: "Blood Pressure",
-                                unit: " / 80 mmhg",
-                                systemIcon: "thermometer.variable",
-                                value: .constant("") // FIXME: Change this
-                            )
-                        }
-                    }
-                }
-                
-                
-                // FIXME: Tasks color matching
-                VStack(spacing: 24) {
-                    HStack {
-                        Text("Tasks")
-                            .font(.title)
-                            .bold()
-                        
-                        Spacer()
-                    }
-                }
-            case .note:
-                // FIXME: Note color matching
-                Text("Notes")
-                    .font(.title)
-                    .bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                ZStack(alignment: .bottomTrailing) {
-                    RoundedRectangle(cornerRadius: 16)
-                        .fill(Color.yellow.opacity(0.15))
-                    
-                    VStack(alignment: .leading, spacing: 0) {
-                        TextEditor(text: $noteText)
-                            .scrollContentBackground(.hidden)
-                            .padding(16)
-                            .onChange(of: noteText) {
-                                lastEditedDate = Date()
-                            }
-                        if let date = lastEditedDate {
-                            Text("Last edited: \(date.formatted(date: .abbreviated, time: .shortened))")
+                            if let date = vitalFilledDate {
+                                HStack {
+                                    Image(systemName: "clock")
+                                    Text("Filled at \(date.formatted(date: .omitted, time: .shortened))")
+                                }
                                 .font(.caption)
-                                .foregroundStyle(.secondary)
-                                .padding([.bottom, .leading], 16)
-                        }
+                                .padding(5)
+                                .foregroundStyle(Color.gray)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                }
+                                }
+                            .padding(12)
+                            .background(Color.gray.opacity(0.1))
+                            .cornerRadius(12)
                     }
                     
+                    
+                    // FIXME: Tasks color matching
+                    VStack(spacing: 24) {
+                        HStack {
+                            Text("Tasks")
+                                .font(.title)
+                                .bold()
+                            
+                            Spacer()
+                        }
+                    }
+                case .note:
+                    // FIXME: Note color matching
+                    Text("Notes")
+                        .font(.title)
+                        .bold()
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    ZStack(alignment: .bottomTrailing) {
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(Color.appThird.opacity(0.15))
+                        
+                        VStack(alignment: .leading, spacing: 0) {
+                            TextEditor(text: $noteText)
+                                .scrollContentBackground(.hidden)
+                                .padding(16)
+                                .onChange(of: noteText) {
+                                    lastEditedDate = Date()
+                                }
+                            if let date = lastEditedDate {
+                                Text("Last edited: \(date.formatted(date: .abbreviated, time: .shortened))")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                                    .padding([.bottom, .leading], 16)
+                            }
+                        }
+                        
+                    }
+                    .frame(maxHeight: .infinity)
+                    
                 }
-                .frame(maxHeight: .infinity)
-                
-            }
+            
             
             Spacer()
         }
@@ -139,7 +164,7 @@ struct RoutineDetailView: View {
                         .multilineTextAlignment(.center)
                     Text(Date().formatted(date: .long, time: .omitted))
                         .font(.caption)
-                        .foregroundStyle(.secondary)
+                        .foregroundStyle(.appThird)
                         
                         
                 }
