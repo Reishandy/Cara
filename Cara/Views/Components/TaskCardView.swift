6cb9b756c57d5e46c5e7f419a977ec5eeb023795
KennardMB <73460518+KennardMB@users.noreply.github.com>
2026-06-03 12:02:47 +0800

    added state to TaskCardView (#5)

--- Cara/Views/Components/TaskCardView.swift
+++ Cara/Views/Components/TaskCardView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct TaskCardView: View {
-    @State var taskName: String = "this is an example of long text"
+    @State var taskName: String = "Task Name"
     @State var taskDesc: String = "Task Description"
     @State var taskIconEach: String = "pill.circle.fill"
-    @State var taskAmount: Int = 0
     @State var isChecked: Bool = false
     
     
     var body: some View {
+        if !isChecked {
             HStack(spacing: 20){
                 TaskIconView(taskIcon: taskIconEach)
                 VStack (alignment: .leading){
@@ -30,24 +30,63 @@ struct TaskCardView: View {
                 Spacer()
                 
                 Button {
-                    isChecked.toggle()
+                    withAnimation(.easeInOut(duration: 0)) {
+                        isChecked.toggle()}
                 } label: {
-                    Image(systemName: isChecked ? "checkmark.circle.fill" : "plus.circle")
+                    Image(systemName: "plus.circle")
                         .font(.system(size: 30))
                         .foregroundStyle(Color("AppThirdColor"))
                     
                 }
-                .buttonStyle(PlainButtonStyle())
+                .buttonStyle(.plain)
                 
             }
             .frame(maxWidth: .infinity,
-                   maxHeight:40,
+                   minHeight: 30,
+                   maxHeight: 50,
                    alignment: .leading)
             .padding()
             .background(Color("CapsuleColor"))
             .clipShape(RoundedRectangle(cornerRadius: 20))
             .padding(.horizontal)
-            
+
+        } else {
+            HStack(spacing: 20){
+                TaskIconView(taskIcon: taskIconEach)
+                VStack (alignment: .leading){
+                    Text(taskName)
+                        .multilineTextAlignment(.leading)
+                        .foregroundStyle(Color("AppPrimaryColor"))
+                        .font(.system(size: 20, weight: .medium, design: .default))
+                        .frame(maxWidth: 170, maxHeight: .infinity, alignment: .leading)
+                        
+                }
+                
+                Spacer()
+                
+                Button {
+                    withAnimation(.easeInOut(duration: 0)) {
+                        isChecked.toggle()}
+                } label: {
+                    Image(systemName: "checkmark.circle.fill")
+                        .font(.system(size: 30))
+                        .foregroundStyle(Color("AppThirdColor"))
+                    
+                }
+                .buttonStyle(.plain)
+                
+            }
+            .frame(maxWidth: .infinity,
+                   minHeight: 30,
+                   maxHeight: 50,
+                   alignment: .leading)
+            .padding()
+            .background(Color("SelectedColor"))
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .padding(.horizontal)
+
+        }
+                        
         
         
     }
