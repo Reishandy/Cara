f7bda66c44155e1b00900e5c0258c2dc82e373c6
KennardMB <kennard.mahib@gmail.com>
2026-06-04 17:09:54 +0800

    LearnView

--- Cara/Views/Screens/LearnView.swift
+++ Cara/Views/Screens/LearnView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct LearnView: View {
+    var taskCategory: String = "Task Category"
+    var taskAmount: Int = 0
+    @State private var searchTask = ""
+    let customColor = UIColor(named: "AppPrimaryColor") ?? .systemBlue
+    
+    init() {
+      UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: customColor]
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView(.vertical, showsIndicators: true) {
+                ForEach (0..<10, id: \.self) { _ in
+                    NavigationLink(
+                        value: Screen.taskDetail
+                    ) {
+                        TaskCardView(style: .noButton)
+                    }
+                }
+            }
+            .searchable(text: $searchTask,
+                        placement: .navigationBarDrawer(displayMode: .always),
+                        prompt: "Search Task...")
+        }
+        
+        .navigationTitle("Learn")
+        .navigationSubtitle("Click on a task to learn more")
+        .navigationBarTitleDisplayMode(.large)
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    LearnView()
+    NavigationStack{
+        LearnView()
+    }
 }
