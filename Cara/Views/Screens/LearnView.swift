90349be106ac16c24bcc5397332508a98f67e033
KennardMB <kennard.mahib@gmail.com>
2026-06-04 17:09:39 +0800

    LearnView

---
+++ Cara/Views/Screens/LearnView.swift
@@ -0,0 +1,18 @@
+//
+//  LearnView.swift
+//  Cara
+//
+//  Created by Kennard M on 04/06/26.
+//
+
+import SwiftUI
+
+struct LearnView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearnView()
+}
