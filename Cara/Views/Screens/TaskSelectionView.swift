9b9da14ce3543fdc86d3bc2d6d37f4caf7defb0c
KennardMB <73460518+KennardMB@users.noreply.github.com>
2026-06-04 15:50:35 +0800

    Feature/routine detail edit (#19)
    * RoutineEditDetailView
    
    * TaskSelectionPage
    added search and numbers of save

--- Cara/Views/Screens/TaskSelectionView.swift
+++ Cara/Views/Screens/TaskSelectionView.swift
@@ -9,20 +9,47 @@ import SwiftUI
 
 struct TaskSelectionView: View {
     var taskCategory: String = "Task Category"
+    var taskAmount: Int = 0
+    @State private var searchTask = ""
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: true) {
-            ForEach (0..<10, id: \.self) { _ in
-				NavigationLink(
-					value: Screen.taskDetail
-				) {
-                    TaskCardView()
+        VStack {
+            ScrollView(.vertical, showsIndicators: true) {
+                ForEach (0..<10, id: \.self) { _ in
+                    NavigationLink(
+                        value: Screen.taskDetail
+                    ) {
+                        TaskCardView()
+                    }
                 }
             }
+            .searchable(text: $searchTask, prompt: "Search Task...")
         }
+        .toolbar{
+            ToolbarItem(placement: .bottomBar) {
+                Button {
+                    // FIXME: need to route this to action
+                } label: {
+                    HStack{
+                        Text("\(taskAmount) Selected")
+                        Text("|")
+                        Text("Save")
+                            .font(.headline)
+                    }
+
+                }
+                .frame(maxWidth: .infinity)
+                .buttonStyle(.borderedProminent)
+                .tint(Color("AppThirdColor"))
+            }
+        }
+        .navigationTitle("Add Tasks")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-	TaskSelectionView()
+    NavigationStack{
+        TaskSelectionView()
+    }
 }
