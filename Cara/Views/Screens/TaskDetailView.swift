6a04311b5ba64139f2b2b2b4cc7599379302b424
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-06-10 09:34:09 +0800

    keep appbartitle on taskdetail

--- Cara/Views/Screens/TaskDetailView.swift
+++ Cara/Views/Screens/TaskDetailView.swift
@@ -16,7 +16,6 @@ struct TaskDetailView: View {
 	
 	@Bindable var task: RoutineTask
 	
-	@State private var showAppBarTitle = false
 	@State private var showDeleteConfirmation = false
 	
 	private var isEdit: Bool {
@@ -33,15 +32,6 @@ struct TaskDetailView: View {
 						.frame(height: 160)
 					
 					contentView
-				}
-			}
-			.onScrollGeometryChange(for: CGFloat.self) { geometry in
-				geometry.contentOffset.y
-			} action: { oldOffset, newOffset in
-				if newOffset > 100 {
-					showAppBarTitle = true
-				} else {
-					showAppBarTitle = false
 				}
 			}
 		}
@@ -106,17 +96,13 @@ struct TaskDetailView: View {
 	
 	private var contentView: some View {
 		VStack(alignment: .leading) {
-			if !showAppBarTitle && !isEdit {
-				Text(task.taskName)
-					.font(.title2)
-					.bold()
-					.foregroundStyle(.white)
-					.padding(.horizontal, 16)
-					.padding(.bottom, 4)
-					.shadow(color: .black.opacity(0.8), radius: 10, x: 0, y: 2)
-			} else {
-				Spacer().frame(height: 60)
-			}
+			Text(task.taskName)
+				.font(.title2)
+				.bold()
+				.foregroundStyle(.white)
+				.padding(.horizontal, 16)
+				.padding(.bottom, 4)
+				.shadow(color: .black.opacity(0.8), radius: 10, x: 0, y: 2)
 			
 			VStack(alignment: .leading) {
 				if isEdit {
